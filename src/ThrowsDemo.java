cc19f402f5a4fc926ef55176619dbf536d2a1b91
tve24el024-blip <tve24el024@cet.ac.in>
2026-02-21 01:31:45 +0530

    add

--- src/ThrowsDemo.java
+++ src/ThrowsDemo.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class ThrowsDemo {
-    
-    // TODO: Define static method check(int n) that throws IOException
-    // If n < 0, throw new IOException()
-    // Else, print n
 
-   
-        // TODO: Read n
-        
-        // TODO: try {
-        //     call check(n)
-        // } catch (IOException e) {
-        //     Print "Caught: IO Exception"
-        // }
-    
+    // Method that declares it throws IOException
+    public static void check(int n) throws IOException {
+        if (n < 0) {
+            throw new IOException("Negative number");
+        } else {
+            System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        try {
+            check(n);
+        } catch (IOException e) {
+            System.out.println("Caught: IO Exception");
+        }
+
+        sc.close();
+    }
 }
